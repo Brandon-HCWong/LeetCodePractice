119677c0727260d8f4f946b60e762c2c8a3fdc0f
Brandon <brandon.hungchun.wong@gmail.com>
2023-07-28 17:11:27 +0800

    feat: Add cycle linkedlist related tool functions.

--- app/src/main/java/com/brandonhc/code/LinkedList.kt
+++ app/src/main/java/com/brandonhc/code/LinkedList.kt
@@ -1,25 +1,94 @@
 package com.brandonhc.code
 
-import android.util.Log
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 
     companion object {
-        fun generateFromList(list: List<Int>): ListNode? {
+        fun generate(list: List<Int>): ListNode? {
             if (list.isEmpty()) {
                 return null
             }
-            val dummyNode = ListNode(Int.MIN_VALUE)
-            var currentNode = dummyNode
+            val nodeDummy = ListNode(Int.MIN_VALUE)
+            var nodeCurrent = nodeDummy
             list.forEach {
-                val tempNode = ListNode(it)
-                currentNode.next = tempNode
-                currentNode = tempNode
+                val nodeTemp = ListNode(it)
+                nodeCurrent.next = nodeTemp
+                nodeCurrent = nodeTemp
             }
-            return dummyNode.next
+            return nodeDummy.next
+        }
+
+        fun generateCycle(list: List<Int>, cycleStartIndex: Int): ListNode? {
+            if (list.isEmpty() || cycleStartIndex < 0 || cycleStartIndex >= list.size) {
+                return null
+            }
+            val nodeDummy = ListNode(Int.MIN_VALUE)
+            var nodeCycleStart: ListNode? = null
+            var nodeCurrent = nodeDummy
+            list.forEachIndexed { index, value ->
+                val nodeTemp = ListNode(value)
+                nodeCurrent.next = nodeTemp
+                nodeCurrent = nodeTemp
+                if (index == cycleStartIndex) {
+                    nodeCycleStart = nodeTemp
+                }
+            }
+            nodeCurrent.next = nodeCycleStart
+            return nodeDummy.next
         }
     }
+
+    override fun toString(): String {
+        return if (isCycle()) {
+            findCycle()?.let {
+                return "${flatUntilCycle(it)} : CycleNode = ${it.`val`}"
+            }
+            return "Something wrong"
+        } else {
+            flat().toString()
+        }
+    }
+}
+
+fun ListNode?.isCycle(): Boolean = findCycle() != null
+fun ListNode?.findCycle(): ListNode? {
+    if (this == null) {
+        return null
+    }
+
+    var nodeSlow: ListNode? = this
+    var nodeFast: ListNode? = this
+
+    while (nodeFast?.next != null) {
+        nodeSlow = nodeSlow?.next
+        nodeFast = nodeFast.next?.next
+        if (nodeSlow == nodeFast) {
+            nodeSlow = this
+            while (nodeSlow != nodeFast) {
+                nodeSlow = nodeSlow?.next
+                nodeFast = nodeFast?.next
+            }
+            return nodeSlow
+        }
+    }
+    return null
+}
+fun ListNode?.flatUntilCycle(cycleStartNode: ListNode): List<Int> {
+    val result = arrayListOf<Int>()
+    var tempNode: ListNode? = this
+    var countMeetCycle = 0
+    while (tempNode != null) {
+        if (tempNode == cycleStartNode) {
+            countMeetCycle++
+            if (countMeetCycle == 2) {
+                break
+            }
+        }
+        result.add(tempNode.`val`)
+        tempNode = tempNode.next
+    }
+    return result
 }
 
 fun ListNode?.flat(): List<Int> {
