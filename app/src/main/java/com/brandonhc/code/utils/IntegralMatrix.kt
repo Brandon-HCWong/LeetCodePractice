5c617cad6dc8ed6e52ec1a5ebb4697151adc8be9
Brandon <brandon.hungchun.wong@gmail.com>
2023-10-04 14:39:38 +0800

    refactor: Add some logic to cover more scenario.

--- app/src/main/java/com/brandonhc/code/utils/IntegralMatrix.kt
+++ app/src/main/java/com/brandonhc/code/utils/IntegralMatrix.kt
@@ -1,6 +1,6 @@
 package com.brandonhc.code.utils
 
-class IntegralMatrix(val matrix: Array<IntArray>) {
+class IntegralMatrix(private val matrix: Array<IntArray>) {
 
     constructor(array: IntArray) : this(arrayOf(array))
 
@@ -20,9 +20,19 @@ class IntegralMatrix(val matrix: Array<IntArray>) {
     }
 
     fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
-        return integralMatrix[row1][col1] -
-                integralMatrix[row1][col2+1] -
-                integralMatrix[row2+1][col1] +
-                integralMatrix[row2+1][col2+1]
+        val (largeRow, smallRow) = if (row1 > row2) {
+            row1+1 to row2
+        } else {
+            row2+1 to row1
+        }
+        val (largeCol, smallCol) = if (col1 > col2) {
+            col1+1 to col2
+        } else {
+            col2+1 to col1
+        }
+        return integralMatrix[largeRow][largeCol] -
+                integralMatrix[largeRow][smallCol] -
+                integralMatrix[smallRow][largeCol] +
+                integralMatrix[smallRow][smallCol]
     }
 }
