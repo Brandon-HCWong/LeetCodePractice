e4b94210b9f3b55c7748b03f5aa7764238eae6e5
Brandon <brandon.hungchun.wong@gmail.com>
2023-12-04 15:00:42 +0800

    feat: Add No.0026 problem implementation and related test cases.

---
+++ app/src/main/java/com/brandonhc/code/leetcode/array/Code0026.kt
@@ -0,0 +1,23 @@
+package com.brandonhc.code.leetcode.array
+
+
+/**
+ * ## Easy
+ * ## 26. Remove Duplicates from Sorted Array
+ * ## [Website Link](https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
+ */
+
+class Code0026 {
+    fun removeDuplicates(nums: IntArray): Int {
+        var indexSlow = 0
+        var indexFast = 0
+        while (indexFast < nums.size) {
+            if (nums[indexSlow] != nums[indexFast]) {
+                indexSlow++
+                nums[indexSlow] = nums[indexFast]
+            }
+            indexFast++
+        }
+        return indexSlow + 1
+    }
+}
