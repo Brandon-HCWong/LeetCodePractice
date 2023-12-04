2e1b25d395d97e3f5db1b945ff77b7dd11f06104
Brandon <brandon.hungchun.wong@gmail.com>
2023-12-04 17:19:05 +0800

    feat: Add No.0027 problem implementation and related test cases.

--- app/src/test/java/com/brandonhc/code/leetcode/array/Code0027Test.kt
+++ app/src/test/java/com/brandonhc/code/leetcode/array/Code0027Test.kt
@@ -3,16 +3,17 @@ package com.brandonhc.code.leetcode.array
 import org.junit.Assert
 import org.junit.Test
 
-class Code0026Test {
+class Code0027Test {
     @Test
     fun test_Case1() {
         // Arrange
-        val nums = intArrayOf(1,1,2)
+        val nums = intArrayOf(3,2,2,3)
+        val removedVal = 3
         val answerNum = 2
-        val answerArray = intArrayOf(1,2)
+        val answerArray = intArrayOf(2,2)
 
         // Act
-        val resultNum = Code0026().removeDuplicates(nums)
+        val resultNum = Code0027.removeElement(nums, removedVal)
         val resultArray = nums.copyOfRange(0, resultNum)
 
         // Assert
@@ -25,12 +26,13 @@ class Code0026Test {
     @Test
     fun test_Case2() {
         // Arrange
-        val nums = intArrayOf(0,0,1,1,1,2,2,3,3,4)
+        val nums = intArrayOf(0,1,2,2,3,0,4,2)
+        val removedVal = 2
         val answerNum = 5
-        val answerArray = intArrayOf(0,1,2,3,4)
+        val answerArray = intArrayOf(0,1,4,0,3)
 
         // Act
-        val resultNum = Code0026().removeDuplicates(nums)
+        val resultNum = Code0027.removeElement(nums, removedVal)
         val resultArray = nums.copyOfRange(0, resultNum)
 
         // Assert
