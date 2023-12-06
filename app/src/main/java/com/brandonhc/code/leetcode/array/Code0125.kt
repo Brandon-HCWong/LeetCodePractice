da491ecc713574c3dccdfd0e7cf6bf7787923971
Brandon <brandon.hungchun.wong@gmail.com>
2023-12-06 16:28:04 +0800

    feat: Add No.0125 problem implementation and related test cases.

---
+++ app/src/main/java/com/brandonhc/code/leetcode/array/Code0125.kt
@@ -0,0 +1,36 @@
+package com.brandonhc.code.leetcode.array
+
+
+/**
+ * ## Easy
+ * ## 125. Valid Palindrome
+ * ## [Website Link](https://leetcode.com/problems/valid-palindrome/)
+ */
+
+object Code0125 {
+    fun isPalindrome(s: String): Boolean {
+        val str = s.lowercase().filter {
+            it.isLetterOrDigit()
+        }
+        var indexL = 0
+        var indexR = 0
+        when (str.length % 2 == 1) {
+            true -> {
+                indexL = str.length / 2
+                indexR = str.length / 2
+            }
+            false -> {
+                indexL = (str.length-1) / 2
+                indexR = (str.length-1) / 2 + 1
+            }
+        }
+        println("str = $str, length = ${str.length}, indexL = ($indexL, $indexR)")
+        while (indexL >= 0 && indexR < str.length) {
+            if (str[indexL] != str[indexR])
+                return false
+            indexL--
+            indexR++
+        }
+        return true
+    }
+}
