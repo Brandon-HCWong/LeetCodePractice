cd98fa14e2a793f4cf184bd5a74c8d7668320995
Brandon <brandon.hungchun.wong@gmail.com>
2023-12-15 18:15:52 +0800

    feat: Add No.0131 problem implementation and related test cases.

---
+++ app/src/test/java/com/brandonhc/code/leetcode/array/Code0131Test.kt
@@ -0,0 +1,41 @@
+package com.brandonhc.code.leetcode.array
+
+import org.junit.Assert
+import org.junit.Test
+
+class Code0131Test {
+    @Test
+    fun test_Case1() {
+        // Arrange
+        val chars = "aab"
+        val answer = listOf(
+            listOf("a","a","b"),
+            listOf("aa","b"),
+        )
+
+        // Act
+        val result = Code0131.partition(chars)
+
+        // Assert
+        println("[test_Case1] answer = $answer")
+        println("[test_Case1] result = $result")
+        Assert.assertEquals(answer, result)
+    }
+
+    @Test
+    fun test_Case2() {
+        // Arrange
+        val chars = "a"
+        val answer = listOf(
+            listOf("a")
+        )
+
+        // Act
+        val result = Code0131.partition(chars)
+
+        // Assert
+        println("[test_Case2] answer = $answer")
+        println("[test_Case2] result = $result")
+        Assert.assertEquals(answer, result)
+    }
+}
